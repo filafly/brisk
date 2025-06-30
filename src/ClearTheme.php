e25770be9db18cdf89b9cbbaeb67482b993ee779
Nolan Nordlund <nolan@kirschbaumdevelopment.com>
2025-06-30 12:13:58 -0500

    Extending theme core

--- src/ClearTheme.php
+++ src/ClearTheme.php
@@ -2,77 +2,15 @@
 
 namespace Filafly\Themes\Clear;
 
-use Filafly\Icons\Phosphor\PhosphorIcons;
-use Filament\Actions\Action;
-use Filament\Contracts\Plugin;
+use Filafly\Themes\Core\ThemeCore;
 use Filament\Panel;
-use Filament\Tables\Table;
 
-class ClearTheme implements Plugin
+class ClearTheme extends ThemeCore
 {
-    public bool $withSuggestedFont = true;
+    public string $identifier = 'filafly-clear-theme';
 
-    public bool $withSuggestedIcons = true;
-
-    public function getId(): string
+    public function registerTheme(Panel $panel): void
     {
-        return 'clear-theme';
-    }
-
-    public static function get(): static
-    {
-        return filament(app(static::class)->getId());
-    }
-
-    public static function make(): static
-    {
-        return app(static::class);
-    }
-
-    public function register(Panel $panel): void
-    {
-        if ($this->withSuggestedIcons) {
-            $panel->plugin(
-                PhosphorIcons::make()
-                    ->duotone()
-                    ->overrideStyleForIcon([
-                        'phosphor-caret-up',
-                        'phosphor-caret-down',
-                        'phosphor-caret-left',
-                        'phosphor-caret-right',
-                        'phosphor-x',
-                    ], 'regular')
-            );
-        }
-
-        if ($this->withSuggestedFont) {
-            $panel->font('Figtree');
-        }
-
-    }
-
-    public function boot(Panel $panel): void
-    {
-        static::configure();
-    }
-
-    public static function configure(): void
-    {
-        Table::configureUsing(fn (Table $table) => $table->striped());
-        Action::macro('detached', fn () => $this->extraModalWindowAttributes(['class' => 'detached-slide-over']));
-    }
-
-    public function withoutSuggestedFont(): static
-    {
-        $this->withSuggestedFont = false;
-
-        return $this;
-    }
-
-    public function withoutSuggestedIcons(): static
-    {
-        $this->withSuggestedIcons = false;
-
-        return $this;
+        // Theme configuration
     }
 }
